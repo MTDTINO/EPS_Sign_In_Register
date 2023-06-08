1852da6b405d57f856cc376b54be4b99591ec9c9
MTDTINO <103025712+MTDTINO@users.noreply.github.com>
2023-06-08 09:44:59 +0200

    Scaffolding

---
+++ Time-Off Register/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DateTimePicker1 = New DateTimePicker()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(88, 29)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(375, 23)
+        DateTimePicker1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(26, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(31, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Date"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(26, 74)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(46, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Time In"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(26, 116)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(56, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Time Out"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(88, 66)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(375, 23)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(88, 108)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(375, 23)
+        TextBox2.TabIndex = 6
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4})
+        DataGridView1.Location = New Point(21, 197)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(442, 236)
+        DataGridView1.TabIndex = 10
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Date"
+        Column1.Name = "Column1"' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Time In"
+        Column2.Name = "Column2"' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Time Out"
+        Column3.Name = "Column3"' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Time Owed"
+        Column4.Name = "Column4"' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(114, 483)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 11
+        Button1.Text = "APPEND"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(297, 483)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 12
+        Button2.Text = "CLEAR"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(206, 483)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 13
+        Button3.Text = "EXPORT"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(61, 163)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(48, 19)
+        RadioButton1.TabIndex = 14
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "PYEI"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(206, 163)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(58, 19)
+        RadioButton2.TabIndex = 15
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "WCED"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(363, 163)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(41, 19)
+        RadioButton3.TabIndex = 16
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "GA"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(483, 585)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(DateTimePicker1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+End Class
---
+++ Time-Off Register/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+    End Sub
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim timeIn As DateTime = DateTime.Parse(TextBox1.Text)
+        Dim timeOut As DateTime = DateTime.Parse(TextBox2.Text)
+
+        'PYEI Start and End Times
+        Dim startTime_pyei As DateTime = DateTime.Parse("07:00:00")
+        Dim endTime_mon_thurs As DateTime = DateTime.Parse("15:15:00")
+        Dim endTime_frid As DateTime = DateTime.Parse("14:00:00")
+
+        'WCED Start and End Times
+        Dim wced_startTime As DateTime = DateTime.Parse("07:20:00")
+        Dim wced_endTime_mon_wed As DateTime = DateTime.Parse("15:00:00")
+        Dim wced_endTime_thurs As DateTime = DateTime.Parse("15:15:00")
+        Dim wced_endTime_fri As DateTime = DateTime.Parse("13:00:00")
+
+        Dim selectedDate As DateTime = DateTimePicker1.Value
+
+        ' PYEI
+        If RadioButton1.Checked = True AndAlso selectedDate.DayOfWeek >= DayOfWeek.Monday AndAlso selectedDate.DayOfWeek <= DayOfWeek.Thursday And (timeIn >= startTime_pyei) Then
+            Dim partTimeOwed As TimeSpan = timeIn - startTime_pyei
+            Dim timeCompleted As TimeSpan = timeOut - endTime_mon_thurs
+
+            Dim TimeOwed As TimeSpan = timeCompleted - partTimeOwed
+            DataGridView1.Rows.Add(DateTimePicker1.Text, TextBox1.Text, TextBox2.Text, TimeOwed)
+        End If
+
+        If RadioButton1.Checked = True AndAlso selectedDate.DayOfWeek = DayOfWeek.Friday AndAlso (timeIn >= startTime_pyei) Then
+            Dim partTimeOwed As TimeSpan = timeIn - startTime_pyei
+            Dim timeCompleted As TimeSpan = timeOut - endTime_frid
+
+            Dim TimeOwed As TimeSpan = timeCompleted - partTimeOwed
+            DataGridView1.Rows.Add(DateTimePicker1.Text, TextBox1.Text, TextBox2.Text, TimeOwed)
+        End If
+
+        'WCED
+        If RadioButton2.Checked AndAlso selectedDate.DayOfWeek >= DayOfWeek.Monday AndAlso selectedDate.DayOfWeek <= DayOfWeek.Wednesday Then
+            Dim partTimeOwed As TimeSpan = timeIn - wced_startTime
+            Dim timeCompleted As TimeSpan = timeOut - wced_endTime_mon_wed
+
+            Dim TimeOwed As TimeSpan = timeCompleted - partTimeOwed
+            DataGridView1.Rows.Add(DateTimePicker1.Text, TextBox1.Text, TextBox2.Text, TimeOwed)
+
+        ElseIf RadioButton2.Checked AndAlso selectedDate.DayOfWeek = DayOfWeek.Thursday AndAlso (timeIn >= wced_startTime) Then
+            Dim partTimeOwed As TimeSpan = timeIn - wced_startTime
+            Dim timeCompleted As TimeSpan = timeOut - wced_endTime_thurs
+
+            Dim TimeOwed As TimeSpan = timeCompleted - partTimeOwed
+            DataGridView1.Rows.Add(DateTimePicker1.Text, TextBox1.Text, TextBox2.Text, TimeOwed)
+
+        ElseIf RadioButton2.Checked = True AndAlso selectedDate.DayOfWeek = DayOfWeek.Friday AndAlso (timeIn >= wced_startTime) Then
+            Dim partTimeOwed As TimeSpan = timeIn - wced_startTime
+            Dim timeCompleted As TimeSpan = timeOut - wced_endTime_fri
+
+            Dim TimeOwed As TimeSpan = timeCompleted - partTimeOwed
+            DataGridView1.Rows.Add(DateTimePicker1.Text, TextBox1.Text, TextBox2.Text, TimeOwed)
+        End If
+    End Sub
+End Class
