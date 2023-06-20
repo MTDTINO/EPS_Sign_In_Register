8f55e23039a7731f67b73bf6e8421f4866a4b899
MTDTINO <103025712+MTDTINO@users.noreply.github.com>
2023-06-20 12:26:19 +0200

    layout

--- Time-Off Register/Form1.Designer.vb
+++ Time-Off Register/Form1.Designer.vb
@@ -27,10 +27,6 @@ Partial Class Form1
         Label2 = New Label()
         Label3 = New Label()
         DataGridView1 = New DataGridView()
-        Column1 = New DataGridViewTextBoxColumn()
-        Column2 = New DataGridViewTextBoxColumn()
-        Column3 = New DataGridViewTextBoxColumn()
-        Column4 = New DataGridViewTextBoxColumn()
         Button1 = New Button()
         Button2 = New Button()
         Button3 = New Button()
@@ -39,12 +35,23 @@ Partial Class Form1
         RadioButton3 = New RadioButton()
         DateTimePicker2 = New DateTimePicker()
         DateTimePicker3 = New DateTimePicker()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        Column5 = New DataGridViewTextBoxColumn()
+        Column6 = New DataGridViewTextBoxColumn()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' DateTimePicker1
         ' 
-        DateTimePicker1.Location = New Point(88, 29)
+        DateTimePicker1.Location = New Point(129, 31)
         DateTimePicker1.Name = "DateTimePicker1"
         DateTimePicker1.Size = New Size(375, 23)
         DateTimePicker1.TabIndex = 0
@@ -52,7 +59,7 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(26, 35)
+        Label1.Location = New Point(67, 37)
         Label1.Name = "Label1"
         Label1.Size = New Size(31, 15)
         Label1.TabIndex = 2
@@ -60,7 +67,7 @@ Partial Class Form1
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(26, 74)
+        Label2.Location = New Point(67, 76)
         Label2.Name = "Label2"
         Label2.Size = New Size(46, 15)
         Label2.TabIndex = 3
@@ -68,7 +75,7 @@ Partial Class Form1
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(26, 116)
+        Label3.Location = New Point(67, 118)
         Label3.Name = "Label3"
         Label3.Size = New Size(56, 15)
         Label3.TabIndex = 4
@@ -78,12 +85,140 @@ Partial Class Form1
         DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
         DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4})
-        DataGridView1.Location = New Point(12, 197)
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4, Column5, Column6})
+        DataGridView1.Location = New Point(6, 188)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowTemplate.Height = 25
-        DataGridView1.Size = New Size(459, 236)
+        DataGridView1.Size = New Size(570, 236)
         DataGridView1.TabIndex = 10
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Button1.Location = New Point(18, 473)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(123, 23)
+        Button1.TabIndex = 11
+        Button1.Text = "APPEND"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Button2.Location = New Point(323, 473)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(253, 23)
+        Button2.TabIndex = 12
+        Button2.Text = "CLEAR TABLE"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        Button3.Location = New Point(147, 473)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(170, 23)
+        Button3.TabIndex = 13
+        Button3.Text = "EXPORT"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(113, 163)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(48, 19)
+        RadioButton1.TabIndex = 14
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "PYEI"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(258, 163)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(58, 19)
+        RadioButton2.TabIndex = 15
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "WCED"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(415, 163)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(41, 19)
+        RadioButton3.TabIndex = 16
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "GA"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' DateTimePicker2
+        ' 
+        DateTimePicker2.Format = DateTimePickerFormat.Time
+        DateTimePicker2.Location = New Point(129, 70)
+        DateTimePicker2.Name = "DateTimePicker2"
+        DateTimePicker2.ShowUpDown = True
+        DateTimePicker2.Size = New Size(375, 23)
+        DateTimePicker2.TabIndex = 17
+        DateTimePicker2.TabStop = False
+        ' 
+        ' DateTimePicker3
+        ' 
+        DateTimePicker3.Format = DateTimePickerFormat.Time
+        DateTimePicker3.Location = New Point(129, 112)
+        DateTimePicker3.Name = "DateTimePicker3"
+        DateTimePicker3.ShowUpDown = True
+        DateTimePicker3.Size = New Size(375, 23)
+        DateTimePicker3.TabIndex = 17
+        DateTimePicker3.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(13, 454)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(60, 15)
+        Label4.TabIndex = 18
+        Label4.Text = "File Name"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(79, 444)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(238, 23)
+        TextBox1.TabIndex = 19
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(323, 444)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(253, 23)
+        Button4.TabIndex = 20
+        Button4.Text = "CLEAR"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(18, 502)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(558, 23)
+        Button5.TabIndex = 21
+        Button5.Text = "Time Owed"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(18, 531)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(558, 23)
+        Button6.TabIndex = 22
+        Button6.Text = "TTO"
+        Button6.UseVisualStyleBackColor = True
         ' 
         ' Column1
         ' 
@@ -99,100 +234,27 @@ Partial Class Form1
         Column3.Name = "Column3"' 
         ' Column4
         ' 
-        Column4.HeaderText = "Time Owed"
+        Column4.HeaderText = "Time"
         Column4.Name = "Column4"' 
-        ' Button1
+        ' Column5
         ' 
-        Button1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
-        Button1.Location = New Point(114, 483)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(75, 23)
-        Button1.TabIndex = 11
-        Button1.Text = "APPEND"
-        Button1.UseVisualStyleBackColor = True
+        Column5.HeaderText = "Time Owed"
+        Column5.Name = "Column5"' 
+        ' Column6
         ' 
-        ' Button2
-        ' 
-        Button2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
-        Button2.Location = New Point(297, 483)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(75, 23)
-        Button2.TabIndex = 12
-        Button2.Text = "CLEAR"
-        Button2.UseVisualStyleBackColor = True
-        ' 
-        ' Button3
-        ' 
-        Button3.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
-        Button3.Location = New Point(206, 483)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(75, 23)
-        Button3.TabIndex = 13
-        Button3.Text = "EXPORT"
-        Button3.UseVisualStyleBackColor = True
-        ' 
-        ' RadioButton1
-        ' 
-        RadioButton1.Anchor = AnchorStyles.Left Or AnchorStyles.Right
-        RadioButton1.AutoSize = True
-        RadioButton1.Location = New Point(61, 163)
-        RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(48, 19)
-        RadioButton1.TabIndex = 14
-        RadioButton1.TabStop = True
-        RadioButton1.Text = "PYEI"
-        RadioButton1.UseVisualStyleBackColor = True
-        ' 
-        ' RadioButton2
-        ' 
-        RadioButton2.Anchor = AnchorStyles.Left Or AnchorStyles.Right
-        RadioButton2.AutoSize = True
-        RadioButton2.Location = New Point(206, 163)
-        RadioButton2.Name = "RadioButton2"
-        RadioButton2.Size = New Size(58, 19)
-        RadioButton2.TabIndex = 15
-        RadioButton2.TabStop = True
-        RadioButton2.Text = "WCED"
-        RadioButton2.UseVisualStyleBackColor = True
-        ' 
-        ' RadioButton3
-        ' 
-        RadioButton3.Anchor = AnchorStyles.Left Or AnchorStyles.Right
-        RadioButton3.AutoSize = True
-        RadioButton3.Location = New Point(363, 163)
-        RadioButton3.Name = "RadioButton3"
-        RadioButton3.Size = New Size(41, 19)
-        RadioButton3.TabIndex = 16
-        RadioButton3.TabStop = True
-        RadioButton3.Text = "GA"
-        RadioButton3.UseVisualStyleBackColor = True
-        ' 
-        ' DateTimePicker2
-        ' 
-        DateTimePicker2.Format = DateTimePickerFormat.Time
-        DateTimePicker2.Location = New Point(88, 68)
-        DateTimePicker2.Name = "DateTimePicker2"
-        DateTimePicker2.ShowUpDown = True
-        DateTimePicker2.Size = New Size(375, 23)
-        DateTimePicker2.TabIndex = 17
-        DateTimePicker2.TabStop = False
-        ' 
-        ' DateTimePicker3
-        ' 
-        DateTimePicker3.Format = DateTimePickerFormat.Time
-        DateTimePicker3.Location = New Point(88, 110)
-        DateTimePicker3.Name = "DateTimePicker3"
-        DateTimePicker3.ShowUpDown = True
-        DateTimePicker3.Size = New Size(375, 23)
-        DateTimePicker3.TabIndex = 17
-        DateTimePicker3.TabStop = False
-        ' 
+        Column6.HeaderText = "TTO"
+        Column6.Name = "Column6"' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         AutoValidate = AutoValidate.EnablePreventFocusChange
-        ClientSize = New Size(483, 585)
+        ClientSize = New Size(588, 585)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
         Controls.Add(DateTimePicker3)
         Controls.Add(DateTimePicker2)
         Controls.Add(RadioButton3)
@@ -219,10 +281,6 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
@@ -231,4 +289,15 @@ Partial Class Form1
     Friend WithEvents RadioButton3 As RadioButton
     Friend WithEvents DateTimePicker2 As DateTimePicker
     Friend WithEvents DateTimePicker3 As DateTimePicker
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
